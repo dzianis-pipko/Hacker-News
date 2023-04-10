3964c65517bc359a79d553952cd33e0b9f31150f
Denis Pipko <dzianis.pipko@gmail.com>
2023-04-10 15:48:56 +0300

    refactoring files: index.ts, Root... StoriesCo...

--- src/components/StoriesContainer.tsx
+++ src/components/StoriesContainer.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { Suspense, useCallback, useEffect, useState } from "react";
 import { getStoryAll, getStoryIds } from "../services/api";
 import { Stories } from "./Stories";
 import { Loader } from "./Loader";
@@ -7,54 +7,102 @@ import { slicingStoryIds } from "../utils/slicingStories";
 import { sortingStories } from "../utils/sortingStories";
 import { Button } from "@mui/material";
 import { ONE_MIN } from "../constants";
+import { Await, defer, useLoaderData } from "react-router-dom";
 
 export const StoriesContainer = () => {
-  const [stories, setStories] = useState<StoryItem[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  // const [stories, setStories] = useState<StoryItem[]>([]);
+  // const [isLoading, setIsLoading] = useState<boolean>(false);
   const [toggleUpdateStories, setToggleUpdateStories] = useState<boolean>(true);
+  const { stories, refetch }: any = useLoaderData();
 
-  const fetchData = useCallback(async () => {
-    try {
-      setIsLoading(true);
-      const storyIds = await getStoryIds();
-      const storyAll = await getStoryAll(slicingStoryIds(storyIds));
-      setStories(sortingStories(storyAll));
-    } catch (e) {
-      console.error(e);
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
+  // const fetchData = useCallback(async () => {
+  //   try {
+  //     setIsLoading(true);
+  //     const storyIds = await getStoryIds();
+  //     const storyAll = await getStoryAll(slicingStoryIds(storyIds));
+  //     setStories(sortingStories(storyAll));
+  //   } catch (e) {
+  //     console.error(e);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // }, []);
 
   const handleUpdateStories = useCallback(() => {
-    setToggleUpdateStories(!toggleUpdateStories);
+    // setToggleUpdateStories(!toggleUpdateStories);
+    refetch();
   }, [toggleUpdateStories]);
 
-  useEffect(() => {
-    fetchData();
-    const intervalId = setInterval(
-      () => setToggleUpdateStories(!toggleUpdateStories),
-      ONE_MIN
-    );
-    return () => clearInterval(intervalId);
-  }, [toggleUpdateStories]);
+  // useEffect(() => {
+  //   console.log("efect");
+  //   storiesLoader();
+  //   const intervalId = setInterval(
+  //     () => setToggleUpdateStories(!toggleUpdateStories),
+  //     ONE_MIN
+  //   );
+  //   return () => clearInterval(intervalId);
+  // }, [toggleUpdateStories]);
+
+  // useEffect(() => {
+  //   fetchData();
+  //   const intervalId = setInterval(
+  //     () => setToggleUpdateStories(!toggleUpdateStories),
+  //     ONE_MIN
+  //   );
+  //   return () => clearInterval(intervalId);
+  // }, [toggleUpdateStories]);
+
+  // return (
+  //   <>
+  //     {isLoading ? (
+  //       <Loader />
+  //     ) : (
+  //       <>
+  //         <Suspense fallback={<Loader />}>
+  //           <Await></Await>
+  //         </Suspense>
+  //         <Button
+  //           onClick={handleUpdateStories}
+  //           variant="outlined"
+  //           sx={{ mt: 3 }}
+  //         >
+  //           Refresh
+  //         </Button>
+  //         <Stories stories={stories as StoryItem[]} />
+  //       </>
+  //     )}
+  //   </>
+  // );
 
   return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          <Button
-            onClick={handleUpdateStories}
-            variant="outlined"
-            sx={{ mt: 3 }}
-          >
-            Refresh
-          </Button>
-          <Stories stories={stories} />
-        </>
-      )}
-    </>
+    <Suspense fallback={<Loader />}>
+      <Await resolve={stories}>
+        {(resolvedStories) => (
+          <>
+            <Button
+              onClick={handleUpdateStories}
+              variant="outlined"
+              sx={{ mt: 3 }}
+            >
+              Refresh
+            </Button>
+            <Stories stories={resolvedStories as StoryItem[]} />
+          </>
+        )}
+      </Await>
+    </Suspense>
   );
+};
+
+const getStories = async () => {
+  const storyIds = await getStoryIds();
+  const storyAll = await getStoryAll(slicingStoryIds(storyIds));
+  return sortingStories(storyAll);
+};
+
+export const storiesLoader = async () => {
+  console.log("storiesLoader");
+  return defer({
+    stories: getStories(),
+  });
 };
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,15 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import { Layout } from "../components/layout/Layout";
 import { StoryDetailScreen } from "../screens/StoryDetailScreen";
 import { StoriesScreen } from "../screens/StoriesScreen";
+import { storiesLoader } from "../components/StoriesContainer";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<StoriesScreen />} loader={storiesLoader} />
+      <Route path="news/:id" element={<StoryDetailScreen />} />
+    </Route>
+  )
+);
 
 export const RootNavigator = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<StoriesScreen />} />
-        <Route path="news/:id" element={<StoryDetailScreen />} />
-      </Route>
-    </Routes>
-  );
+  return <RouterProvider router={router} />;
 };
